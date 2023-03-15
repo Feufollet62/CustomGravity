27de35d27477cb6e0b081d3716a478b87050fed5
Eliot <fromenteliot@gmail.com>
2023-03-15 16:56:26 +0100

    Janky cam

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -7,8 +7,9 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField, Range(1f, 20f)] private float distance = 5f;
     [SerializeField, Min(0f)] private float focusRadius = 1f;
     [SerializeField, Range(0f, 1f)] float focusCentering = 0.5f;
+    [SerializeField, Range(1f, 360f)] float rotationSpeed = 90f;
     
-    
+    private Vector2 _orbitAngles = new Vector2(45f, 0f);
     private Vector3 _focusPoint;
     
     private void Awake ()
@@ -19,8 +20,12 @@ public class OrbitCamera : MonoBehaviour
     private void LateUpdate()
     {
         UpdateFocusPoint();
+        ManualRotation();
+        
+        Quaternion lookRotation = Quaternion.Euler(_orbitAngles);
         Vector3 lookDirection = transform.forward;
-        transform.localPosition = _focusPoint - lookDirection * distance;
+        Vector3 lookPosition = _focusPoint - lookDirection * distance;
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
     }
     
     private void UpdateFocusPoint ()
@@ -35,5 +40,15 @@ public class OrbitCamera : MonoBehaviour
             _focusPoint = Vector3.Lerp(targetPoint, _focusPoint, t);
         }
         else _focusPoint = targetPoint;
+    }
+    
+    private void ManualRotation()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+        const float e = 0.001f;
+        if (input.x < -e || input.x > e || input.y < -e || input.y > e)
+        {
+            _orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+        }
     }
 }
