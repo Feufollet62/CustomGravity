01310ed9993f0cfde6b6901d6189fcb1ddbf8ba0
Eliot <fromenteliot@gmail.com>
2023-03-15 10:18:41 +0100

    Sliding sphere with acceleration / max speed

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
+    [SerializeField, Range(0f, 20f)] float maxSpeed = 10f;
+    [SerializeField, Range(0f, 50f)] float maxAcceleration = 10f;
+    
+    Vector3 _velocity;
     
     private void Update()
     {
@@ -11,8 +14,14 @@ public class PlayerController : MonoBehaviour
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         
-        Vector3 velocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        Vector3 displacement = velocity * Time.deltaTime;
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        
+        _velocity.x = Mathf.MoveTowards(_velocity.x, desiredVelocity.x, maxSpeedChange);
+        _velocity.z = Mathf.MoveTowards(_velocity.z, desiredVelocity.z, maxSpeedChange);
+        
+        Vector3 displacement = _velocity * Time.deltaTime;
+        
         transform.localPosition += displacement;
     }
 }
