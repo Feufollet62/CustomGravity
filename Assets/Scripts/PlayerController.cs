f5173f2a5dbf881e29c5e8a8df4b62a81b663940
Eliot <fromenteliot@gmail.com>
2023-03-15 10:49:23 +0100

    Added player physic material + file reordering

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Range(0f, 20f)] float maxSpeed = 10f;
     [SerializeField, Range(0f, 50f)] float maxAcceleration = 10f;
     
-    Vector3 _velocity;
+    private Vector3 _velocity;
+    private Rigidbody _rb;
+
+    void Awake() 
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
     
     private void Update()
     {
@@ -13,15 +19,14 @@ public class PlayerController : MonoBehaviour
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        
+
+        _velocity = _rb.velocity;
         Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         
         _velocity.x = Mathf.MoveTowards(_velocity.x, desiredVelocity.x, maxSpeedChange);
         _velocity.z = Mathf.MoveTowards(_velocity.z, desiredVelocity.z, maxSpeedChange);
         
-        Vector3 displacement = _velocity * Time.deltaTime;
-        
-        transform.localPosition += displacement;
+        _rb.velocity = _velocity;
     }
 }
